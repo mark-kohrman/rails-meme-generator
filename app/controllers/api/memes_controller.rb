7cfaaeea5e606524ea889dfe3983d2cd1d8c4461
Mark-Kohrman <mark.kohrman@gmail.com>
2020-11-22 10:31:56 -0600

    adding update meme action

--- app/controllers/api/memes_controller.rb
+++ app/controllers/api/memes_controller.rb
@@ -18,4 +18,15 @@ class Api::MemesController < ApplicationController
     render 'show.json.jb'
   end
 
+  def update
+    @meme = Meme.find_by(id: params[:id])
+
+    @meme.update(
+      top_text: params[:top_text] || @meme.top_text,
+      bottom_text: params[:bottom_text] || @meme.bottom_text,
+      img_url: params[:img_url] || @meme.img_url
+    )
+    render 'show.json.jb'
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
     get "/memes" => "memes#index"
     get "/memes/:id" => "memes#show"
     post "/memes" => "memes#create"
+    patch "/memes/:id" => "memes#update"
+
 
   end
 end
